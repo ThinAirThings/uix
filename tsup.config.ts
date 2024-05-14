f4415ef0124613e5b39bfd673ec1a45bbf3ee4df
irishcarbomb777 <danielclannan@gmail.com>
2024-05-14 11:09:59 -0400

    build: split packages to untangle dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-    entry: ["src/index.ts"],
+
+const baseOptions = {
+    sourcemap: true,
     clean: true,
     shims: true,
     dts: true,
-    format: ["esm", 'cjs'],
-});
+    format: ["cjs", 'esm'],
+} as Partial<Parameters<typeof defineConfig>[0]>;
+
+export default defineConfig([{
+    entry: { index: "src/index.ts" },
+    ...baseOptions
+}, {
+    entry: { "neo4j/index": "src/index.neo4j.ts" },
+    ...baseOptions
+}, {
+    entry: { "nextjs/index": "src/index.nextjs.ts" },
+    ...baseOptions,
+    banner: { js: `'use server'` }
+}, {
+    entry: { "react/index": "src/index.react.ts" },
+    ...baseOptions
+}]);
