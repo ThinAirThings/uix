8664a83bbfebde85a5fbdf73686698135a90f678
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-07-05 17:39:21 -0700

    config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/thinairthings/uix/tree/documentation/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/thinairthings/uix/tree/documentation/docs/',
         },
         theme: {
           customCss: './src/css/custom.css',
