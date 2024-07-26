321b96f537d6f9153ed9baede34351f009194747
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-07-26 15:22:05 -0400

    wipset

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 ## [1.2.5](https://github.com/ThinAirThings/uix/compare/v1.2.4...v1.2.5) (2024-05-23)
 
+## 3.2.0
+
+### Minor Changes
+
+- adds caching to uix
+
 ## 3.1.3
 
 ### Patch Changes
---
+++ docs/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/lucasamberg/uix/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/home/lucasamberg/uix/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/lucasamberg/uix/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/lucasamberg/uix/docs/src/css/custom.css"),
+];
